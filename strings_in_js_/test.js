8cb7dd894070fbb554bd17389e5b00156846cd80
Sushilk <sushilkamble7738@gmail.com>
2025-08-25 22:37:14 +0530

    learnt string concept in javascript

---
+++ strings_in_js_/test.js
@@ -0,0 +1,89 @@
+// Strings
+// creating string
+// let str = "Ganpati Bappa Morya"
+// let str2 = "Ganesha"
+
+
+
+// console.log(str);
+// console.log(str2);
+
+// string length
+
+// console.log(str.length);
+// console.log(str2.length);
+
+// string indices
+// console.log(str[0]);
+// console.log(str[14]);
+
+// Template Literals
+// let name = "Sushil"
+// let specialString = `Hii,This is ${name}`;
+// console.log(typeof specialString);
+
+
+// let obj = {
+//     item: "pen",
+//     price: 10,
+// }
+
+// console.log("The cost of",obj.item,"is", obj.price, "rupees.");
+// console.log(`The cost of ${obj.item} is ${obj.price}`);
+
+// let str = "Sushil \n\tKamble";
+// console.log(str.length);
+
+
+
+// string methods in js
+// These are built-in functions to manipulate a string.
+
+// let str = "Sushil Kamble"
+// new_str = str.toUpperCase()
+
+// console.log(str);
+// console.log(new_str);
+
+// built in function didnt changed existing str but it returned new str and we save into new
+// variable 
+
+// s = '      World of beauty   '
+// new_s = s.trim()
+// console.log(new_s);
+
+// p = s.concat(new_s)
+// console.log(p);
+
+// let str = '0123456';
+// console.log(str.slice(5));
+
+
+// concatenation in string
+let s = "Sushil"
+// let n = "ushil"
+
+// console.log(s.concat(n));
+
+
+
+
+// replace method in string:-
+
+// let str = "hellololo"
+
+// console.log(str.replace('h', 'y'));
+// console.log(str.replace('lo', 'po'));
+
+
+
+// to search value index in string:
+
+// let str = 'I love python';
+// console.log(str.charAt(3));
+
+// let name = prompt("Enter your name: ")
+
+// console.log(`@${name}${name.length}`);
+
+
