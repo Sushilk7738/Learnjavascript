246ce4dc7c76fc1a764b718e780483f0579f20cf
Sushil Kamble <sushilkamble7738@gmail.com>
2025-09-11 15:12:26 +0000

    learnt comparison and datatypes summary of javascript

---
+++ .vscode/comparison.js
@@ -0,0 +1,37 @@
+// console.log('2' > 1);
+
+// console.log("02" > 1);
+
+// when you will compare two datatypes js automatically converts and returns true
+
+
+
+//***do not compare this values 
+
+// console.log(null > 0);
+
+// console.log(null == 0);
+
+// console.log(null >= 0);    //js converts null to zero in comparison okay thats why it returned true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+//  '===' not only checks value but also checks its datatypes
+
+// console.log('3' == 3);    //js converts into number datatype and returns true
+
+// console.log('3' === 3);   // js strictly compares the values
+
+
+
+
+ 
+
+
+
+
+
---
+++ .vscode/datatypes.js
@@ -0,0 +1,73 @@
+// primitive
+
+// 7 types : string, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+
+// const id = Symbol("#$#")
+
+// console.log(typeof(id));
+
+// const anotherID = Symbol("#$#")
+// console.log(id === anotherID);
+
+
+// const BigNumber = 2664547787836578963212365478978965432147893351477522n
+// console.log(typeof BigNumber);
+
+const Temperature = null
+console.log(typeof Temperature);
+
+
+
+// Reference type (Non primitive)
+
+// Array, objects, functions
+
+// Arrays represented as:
+// const heroes = ['Arjuna', 'Karna', 'Bheem', 'Bheeshma']
+
+
+// Objects are represented as:
+
+// let myObj = {
+    
+//     name : 'Sushil',
+//     Designation : 'Software Engineer',
+
+// }
+
+
+
+// functions are represented as:
+const myfunc =function() {
+    console.log("Sushil");
+    
+}
+
+myfunc()
+
+
+
+
+// * Premitive Datatypes
+
+//                   Type                                   typeof
+
+// i)             Number                               number
+// ii)            String                                   string
+// iii)           Boolean                               boolean
+// iv)           Bigint                                   bigint
+// v)            Symbol                                symbol
+// vi)           Null                                      object
+// vii)          Undefined                           undefined
+
+
+// * Non-Premitive OR Referance OR Object datatype
+
+//                   Type                                   typeof
+
+// i)               Object                                object
+// ii)              Array                                  object
+// iii)             Function                            function(object)
