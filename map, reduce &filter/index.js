9ba7b716ae57e793f7af5c60283866d30e5d9619
Sushilk <sushilkamble7738@gmail.com>
2025-10-01 19:00:20 +0530

    learnt the concept of map, filter, and reduce methods in JavaScript

--- map, reduce &filter/index.js
+++ map, reduce &filter/index.js
@@ -26,3 +26,121 @@
 
 // console.log(b);
 
+
+
+
+// const coding = ['js', 'ruby', 'py', 'cpp', 'java']
+
+// const values = coding.forEach((lang)=>{
+    // console.log(lang);
+    
+// })
+// console.log(values);  //returns undefined 
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const updNums = myNums.filter((num) => num > 4)
+
+
+// const updNums = myNums.filter((num)=>(num > 4))
+// console.log(updNums);
+
+
+// const updNums = myNums.filter((num)=>{
+//     return num > 4     //while using '{}' we declares as scope so it has to be return values otherwise it provide empty arr.
+// })
+
+// console.log(updNums);
+
+
+// const newNums = []
+
+// myNums.forEach((num)=>{
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+
+
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+// ];
+
+
+// const userBooks = books.filter((bk)=>(bk.genre === "History"))
+
+// console.log(userBooks);
+
+// const publishBooks = books.filter((bk)=>(
+//     bk.publish >=1995 && bk.genre ==="History"
+// ))
+
+// console.log(publishBooks);
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const updNums = myNums.map((num)=> num + 10)
+// console.log(updNums);
+
+// const newNums = myNums
+//                 .map((num)=>(num * 10))
+//                 .map((num)=>num + 1)
+//                 .filter((num)=>num >40)
+// console.log(newNums);
+
+
+
+
+
+
+
+// reduce in js:-
+
+// const myNums = [1,2,3,4]
+
+// const myTotal = myNums.reduce((acc, currVal)=>{
+//     console.log(`acc: ${acc} currval: ${currVal}`);
+    
+//     return acc + currVal
+// }, 0)
+
+// console.log(myTotal);
+
+
+// const updNums = myNums.reduce((acc, currVal)=> acc + currVal, 0)
+
+// console.log(updNums);
+
+
+const shoppingCart =[
+    {
+        item : 'js course',
+        price : 2999
+    },
+    {
+        item : 'Py course',
+        price : 3999
+    },
+    {
+        item : 'Java course',
+        price : 4999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item)=> acc+ item.price, 0)
+
+console.log(priceToPay);
